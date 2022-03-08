dc12800d22a4e89f894e91395bdc8a52189662d6
MounikaNekkalapu <100415362+MounikaNekkalapu@users.noreply.github.com>
2022-03-08 11:51:16 +0530

    updated

--- Customerservices/src/main/java/com/example/Customer/CustomerApplication.java
+++ Customerservices/src/main/java/com/example/Customer/CustomerApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableAutoConfiguration(
@@ -19,4 +22,12 @@ public class CustomerApplication {
 	public static void main(String[] args) {
 	      SpringApplication.run(CustomerApplication.class, args);
 	}
+	
+	  @Bean
+	  @LoadBalanced
+	  public RestTemplate restTemplate() 
+	  {
+	       return new RestTemplate();
+	  }
+
 }
